02aa32aba0c3eb90ec37adfcf3823a3ae7ea6ce9
macamarambio <marambio.macarena@gmail.com>
2018-01-02 12:53:44 -0300

    subiendo vista splash

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+
+$(document).ready(()=> {
+
+/*Vista splash con duración de 2 a 5 segundos
+		setTimeout(()=> { 
+			$(".content").fadeOut(500);
+		},3000);
+		setTimeout(()=> { 
+			$(".content2").fadeIn(500);
+		});
+
+
+	});
+*/
