4a7ad273e47e30d62d3f3ac77af7000fe1f3f581
macamarambio <marambio.macarena@gmail.com>
2018-01-04 17:08:35 -0300

    subiendo vista escritorio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(()=> {
 	$('.language').hide();
 	$('.login').hide();
@@ -29,8 +28,6 @@ $(document).ready(()=> {
 		$('.login').hide();
 		$('.principal').show();
 	});
-
-/*Select tipos de comida*/
 
 
 
@@ -68,3 +65,73 @@ function filter(id) {
 }
 
 
+/* Intento para ocultar vista splash desde jQuery
+
+	$(document).ready(function() {
+	function splashMobile($, viewport){
+
+		if( viewport.is('<=sm') ) {
+			$('.language').hide();
+			$('.login').hide();
+			$('.principal').hide();
+		
+			setTimeout(()=> {
+				$(".content").fadeOut(500);
+			},3000);
+			setTimeout(()=> {
+				$(".language").fadeIn(500);
+			});
+		}if( viewport.is('>sm') ) {
+			$('.login').show();
+			$('.principal').hide();
+		}
+		}
+
+			$('#btn1').click(()=>{
+				$('.language').hide();
+				$('.login').show();
+				$('.principal').hide();
+			});
+			$('#btnback').click(()=>{
+				$('.language').show();
+				$('.login').hide();
+			});
+
+			$('#iniciar').click(()=>{
+				$('.language').hide();
+				$('.login').hide();
+				$('.principal').show();
+			});
+});
+
+function filter(id) {
+	var filt = $('#selector').val();
+	$('#comida').val('');
+	if(id == 'comida'){
+		$('#comida').show();
+		$('#mexico').hide();
+		$('#japon').hide();
+		$('#thai').hide();
+	}
+	if(id == 'mexico'){
+		$('#comida').hide();
+		$('#mexico').show();
+		$('#japon').hide();
+		$('#thai').hide();
+	}
+	if(id == 'japon'){
+		$('#comida').hide();
+		$('#mexico').hide();
+		$('#japon').show();
+		$('#thai').hide();
+	}
+	if(id == 'thai'){
+		$('#comida').hide();
+		$('#mexico').hide();
+		$('#japon').hide();
+		$('#thai').show();
+	}
+}
+
+
+*/
