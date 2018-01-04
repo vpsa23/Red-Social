368038703bdd74fe97bbd903b68745911184ac10
carolina <carolina.jarpam@gmail.com>
2018-01-04 09:29:56 -0300

    Filtro

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,7 +31,40 @@ $(document).ready(()=> {
 	});
 
 /*Select tipos de comida*/
+
+
+
 });
 
+/*Filtrar Restaurantes*/
+
+function filter(id) {
+	var filt = $('#selector').val();
+	$('#comida').val('');
+	if(id == 'comida'){
+		$('#comida').show();
+		$('#mexico').hide();
+		$('#japon').hide();
+		$('#thai').hide();
+	}
+	if(id == 'mexico'){
+		$('#comida').hide();
+		$('#mexico').show();
+		$('#japon').hide();
+		$('#thai').hide();
+	}
+	if(id == 'japon'){
+		$('#comida').hide();
+		$('#mexico').hide();
+		$('#japon').show();
+		$('#thai').hide();
+	}
+	if(id == 'thai'){
+		$('#comida').hide();
+		$('#mexico').hide();
+		$('#japon').hide();
+		$('#thai').show();
+	}
+}
 
 
